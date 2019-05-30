da5a670dec5b55506616fe1260c7f6ceefa7c64d
beet-aizu <aki.bdash@gmail.com>
2019-05-30 12:05:27 +0900

    add index

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Index</title>
+  </head>
+  <body>
+       <a href="./index.html"> Rating History </a>
+       <a href="./average.html"> Average </a>
+  </body>
+</html>      
 <?php
 echo 'Hello World!';
 ?>
