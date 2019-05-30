c5651b0e549c018776a4c962ad32a06287fb6d9b
beet-aizu <aki.bdash@gmail.com>
2019-05-30 15:04:46 +0900

    add analytics

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-141121686-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-141121686-1');
+</script>
     <meta charset="utf-8">
     <title>Index</title>
   </head>
   <body>
        <a href="./index.html"> Rating History </a> <br>
        <a href="./average.html"> Average </a> <br>
+       <a href="./beetbeet.html"> ??? </a> <br>
+              
+    プライバシー ポリシー:
+    データ収集のために Cookie を使用しています
+    Google アナリティクスを使用しています
+    詳しくは　<a href="https://policies.google.com/technologies/partner-sites?hl=ja">こちら</a>
   </body>
 </html>      
